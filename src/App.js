580620fb07abfe35ea84bb27627fb54a2718b4df
unknown <dusdjeks@naver.com>
2021-10-13 23:44:18 +0900

    using map

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import React from "react";
 
-function Food({name, picture}){
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} />
-  </div>
-}
 
 const foodILike=[
   {
+    id:1,
     name:"Kimchi",
     image:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
   },
   {
+    id:2,
     name:"Bibimbap",
     image:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
   },
   {
+    id:3,
     name:"Samgyeopsal",
     image:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
   },
   {
+    id:4,
     name:"Kimbap",
     image:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
   },
 ]
 
+function Food({name, picture}){
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name} />
+  </div>
+}
+
 function App() {
   return (<div>
       {foodILike.map(dish => 
-      <Food name={dish.name} picture={dish.image}/>
+      <Food key={dish.id} name={dish.name} picture={dish.image}/>
       )}
     </div>);
 }
